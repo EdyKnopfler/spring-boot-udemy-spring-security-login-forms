255b47f8e92fdcbb443c321778b3dec181e75401
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-10 16:58:16 -0300

    Esqueleto do Spring Initializr

---
+++ src/test/java/com/derso/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derso.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
