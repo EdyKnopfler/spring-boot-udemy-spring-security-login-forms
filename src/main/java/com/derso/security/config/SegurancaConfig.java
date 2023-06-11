0761045067f17b9d669d54cef3064092817478d8
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-10 22:29:44 -0300

    Testando com forms

--- src/main/java/com/derso/security/config/SegurancaConfig.java
+++ src/main/java/com/derso/security/config/SegurancaConfig.java
@@ -2,7 +2,12 @@ package com.derso.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -14,16 +19,16 @@ import org.springframework.security.web.SecurityFilterChain;
  * Foi o que usei quando fiz o curso da Alura:
  * https://github.com/EdyKnopfler/alura-spring-aplicacao/blob/main/src/main/java/br/com/pip/mvc/mudi/WebSecurityConfig.java
  * 
- * Por dentro dos métodos seria a mesma coisa... não fossem outros nomes deprecados.
- * 
  * Para não se perder no meio de tanta mudança, bora ler as docs!
  * https://docs.spring.io/spring-security/reference/current/index.html
+ * 
  */
 
 @Configuration
 public class SegurancaConfig {
 	
 	// ESTE é o cara que configura o HttpSecurity agora :)
+	// Segundo as docs, o SecurityFilterChain se integra na cadeia de filtros de servlets
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// Fluent interfaces são a pior merda já inventada #ChangeMyMind
@@ -33,7 +38,25 @@ public class SegurancaConfig {
 					.requestMatchers("/").permitAll()  // Permite a home
 					.anyRequest().authenticated()      // Bloqueia o resto
 			)
+			.formLogin(Customizer.withDefaults())
 			.build();
+	}
+	
+	@Bean
+	public UserDetailsService usuariosComBcrypt() {
+		// Passwords são iguais aos usernames
+		// Encriptados em https://bcrypt-generator.com/
+		UserDetails admin = User.builder()
+			.username("kânia")
+			.password("{bcrypt}$2a$12$.bLRI7BwK/osDYYSCPVtueLLhqq2d3aRMd4y.cefMjUrzceHRwEmW")
+			.roles("USER", "ADMIN")
+			.build();
+		UserDetails user = User.builder()
+			.username("ZÍMza")
+			.password("{bcrypt}$2a$12$qTllQZ7VLakpuNdmJXzs0.IJ23ZyXmX4g.wjap9.iFG6oeHtH/JCO")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 }
